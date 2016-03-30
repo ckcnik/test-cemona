db8586463e5219a81608b439edbcea9bb2df5abb
Денис Суховерхов <at10tion.angry@yandex.ru>
2016-03-30 10:42:22 +0300

    refactoring migrations

--- cemona/migrations/0001_initial.py
+++ cemona/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import django.utils.timezone
 
 
 class Migration(migrations.Migration):
@@ -11,29 +12,140 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='Event',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('code', models.CharField(unique=True, max_length=10)),
+                ('name', models.CharField(max_length=250)),
+            ],
+            options={
+                'verbose_name': 'Event',
+                'verbose_name_plural': 'Events',
+            },
+        ),
+        migrations.CreateModel(
+            name='LoggingModem',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('created', models.DateTimeField(verbose_name='Date created', default=django.utils.timezone.now)),
+            ],
+            options={
+                'verbose_name': 'Logging Modem',
+                'verbose_name_plural': 'Logging Modems',
+            },
+        ),
+        migrations.CreateModel(
+            name='Measurement',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('code', models.CharField(unique=True, max_length=50)),
+                ('name', models.CharField(max_length=250)),
+                ('codeid', models.IntegerField(null=True, blank=True)),
+            ],
+            options={
+                'verbose_name': 'Measurement',
+                'verbose_name_plural': 'Measurements',
+            },
+        ),
+        migrations.CreateModel(
             name='Modem',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', serialize=False, primary_key=True)),
-                ('manufacturer', models.CharField(max_length=50, blank=True, null=True)),
-                ('model', models.CharField(max_length=50, blank=True, null=True)),
-                ('imei', models.CharField(max_length=16, blank=True, null=True)),
-                ('hw', models.CharField(max_length=10, blank=True, null=True)),
-                ('fw', models.CharField(max_length=10, blank=True, null=True)),
-                ('imsi_sim', models.CharField(max_length=20, blank=True, null=True)),
-                ('time_zone', models.CharField(max_length=20, blank=True, null=True)),
-                ('apn', models.CharField(max_length=20, blank=True, null=True)),
-                ('apn_user', models.CharField(max_length=20, blank=True, null=True)),
-                ('apn_password', models.CharField(max_length=20, blank=True, null=True)),
-                ('ras', models.IntegerField(blank=True, null=True)),
-                ('status', models.TextField(blank=True, null=True)),
-                ('test_status', models.TextField(blank=True, null=True)),
-                ('mode', models.TextField(blank=True, null=True)),
-                ('ethernet_test_status', models.TextField(blank=True, null=True)),
-                ('wifi_test_status', models.TextField(blank=True, null=True)),
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('imei', models.BigIntegerField(verbose_name='Modem IMEI')),
+                ('manufacturer', models.CharField(max_length=50, blank=True, verbose_name='Modem manufacturer', null=True)),
+                ('model', models.CharField(max_length=50, blank=True, verbose_name='Modem model', null=True)),
+                ('hw', models.CharField(max_length=10, blank=True, verbose_name='HW modem', null=True)),
+                ('fw', models.CharField(max_length=10, blank=True, verbose_name='FW modem', null=True)),
+                ('imsi', models.CharField(max_length=20, blank=True, verbose_name='Modem IMSI', null=True)),
             ],
             options={
                 'verbose_name': 'Modem',
                 'verbose_name_plural': 'Modems',
             },
+        ),
+        migrations.CreateModel(
+            name='NetworkAdapter',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('device_id', models.CharField(max_length=16, blank=True, verbose_name='Device identifier', null=True)),
+                ('mac', models.CharField(max_length=20)),
+                ('type', models.IntegerField(choices=[(1, 'Ethernet'), (2, 'WiFi')], default=1, verbose_name='Device type')),
+                ('name', models.CharField(max_length=100, blank=True, verbose_name='Device name', null=True)),
+            ],
+            options={
+                'verbose_name': 'Network adapter',
+                'verbose_name_plural': 'Network adapters',
+            },
+        ),
+        migrations.CreateModel(
+            name='Probe',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('sn', models.CharField(unique=True, max_length=50, verbose_name='Serial number')),
+                ('ram_size', models.IntegerField(verbose_name='Memory Size')),
+                ('sd_size', models.IntegerField()),
+                ('os_version', models.CharField(max_length=100)),
+                ('csw_version', models.CharField(max_length=100)),
+            ],
+            options={
+                'verbose_name': 'Probe',
+                'verbose_name_plural': 'Probes',
+            },
+        ),
+        migrations.CreateModel(
+            name='RegisterAction',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('numstatemodem', models.IntegerField()),
+                ('idactionvalue', models.IntegerField()),
+                ('created', models.DateTimeField(verbose_name='Date created', default=django.utils.timezone.now)),
+                ('beggroup', models.IntegerField()),
+                ('curgroup', models.IntegerField(null=True, blank=True)),
+                ('endgroup', models.IntegerField(null=True, blank=True)),
+                ('closed', models.DateTimeField(null=True, blank=True)),
+                ('action', models.ForeignKey(to='cemona.Event')),
+                ('modem', models.ForeignKey(to='cemona.Modem', verbose_name='Modem')),
+            ],
+            options={
+                'verbose_name': 'Event registration',
+                'verbose_name_plural': 'Events registration',
+            },
+        ),
+        migrations.CreateModel(
+            name='RegisterMeasurement',
+            fields=[
+                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('idcodemeasurment', models.IntegerField()),
+                ('created', models.DateTimeField(verbose_name='Date created', default=django.utils.timezone.now)),
+                ('measurement', models.ForeignKey(to='cemona.Measurement', verbose_name='Measurement')),
+            ],
+            options={
+                'verbose_name': 'Modem',
+                'verbose_name_plural': 'Modems',
+            },
+        ),
+        migrations.AddField(
+            model_name='networkadapter',
+            name='probe',
+            field=models.ForeignKey(to='cemona.Probe', blank=True, related_name='net_adapters', verbose_name='Probe', null=True),
+        ),
+        migrations.AddField(
+            model_name='modem',
+            name='probe',
+            field=models.ForeignKey(to='cemona.Probe', blank=True, related_name='modems', verbose_name='Probe', null=True),
+        ),
+        migrations.AddField(
+            model_name='loggingmodem',
+            name='modem',
+            field=models.ForeignKey(to='cemona.Modem', verbose_name='Modem APN'),
+        ),
+        migrations.AddField(
+            model_name='loggingmodem',
+            name='probe',
+            field=models.ForeignKey(to='cemona.Probe', verbose_name='Modem APN'),
+        ),
+        migrations.AlterUniqueTogether(
+            name='registeraction',
+            unique_together=set([('modem', 'numstatemodem', 'action', 'idactionvalue')]),
         ),
     ]
--- documentation/cemona/docs/index.md
+++ documentation/cemona/docs/index.md
@@ -10,9 +10,58 @@
 * [Nginx](http://nginx.org/ru/) - сервер для отдачи статики.
 * [WSGI HTTP Server](http://gunicorn.org/) - для проекта на релизе.
 
-## Project layout
+## SSH доступ на debian-сервер из под ubuntu
 
-    mkdocs.yml    # The configuration file.
-    docs/
-        index.md  # The documentation homepage.
-        ...       # Other markdown pages, images and other files.
+Если из коробки не работает, коннект виснет и закрывается спустя 1-2 минуты. Выполняем комунду соединения с параметрами дебага:
+```
+#!bash
+$ ssh -v cemona@10.1.3.33
+$ ...
+$ debug1: sending SSH2_MSG_KEX_ECDH_INIT
+$ debug1: expecting SSH2_MSG_KEX_ECDH_REPLY
+$ Connection closed by 10.1.3.33
+```
+Если у вас похожая ситуация, делаем все по следующей иснтукции/
+На локальной машине нужно настроить ssh-клиент, [подробнее](http://superuser.com/questions/699530/git-pull-does-nothing-git-push-just-hangs-debug1-expecting-ssh2-msg-kex-ecd).
+Отредактировать файл ```/etc/ssh/ssh_config``` как показано на стеке.
+Мой файл выглядит так:
+
+    Host *
+    #   ForwardAgent no
+    #   ForwardX11 no
+    #   ForwardX11Trusted yes
+    #   RhostsRSAAuthentication no
+    #   RSAAuthentication yes
+    #   PasswordAuthentication yes
+    #   HostbasedAuthentication no
+    #   GSSAPIAuthentication no
+    #   GSSAPIDelegateCredentials no
+    #   GSSAPIKeyExchange no
+    #   GSSAPITrustDNS no
+    #   BatchMode no
+    #   CheckHostIP yes
+    #   AddressFamily any
+    #   ConnectTimeout 0
+    #   StrictHostKeyChecking ask
+    #   IdentityFile ~/.ssh/identity
+    #   IdentityFile ~/.ssh/id_rsa
+    #   IdentityFile ~/.ssh/id_dsa
+    #   Port 22
+    #   Protocol 2,1
+    #   Cipher 3des
+    Ciphers aes128-ctr,aes192-ctr,aes256-ctr,arcfour256,arcfour128,aes128-cbc,3des-cbc
+    MACs hmac-md5,hmac-sha1,umac-64@openssh.com,hmac-ripemd160
+    #   EscapeChar ~
+    #   Tunnel no
+    #   TunnelDevice any:any
+    #   PermitLocalCommand no
+    #   VisualHostKey no
+    #   ProxyCommand ssh -q -W %h:%p gateway.example.com
+    #   RekeyLimit 1G 1h
+    SendEnv LANG LC_*
+    HashKnownHosts yes
+    GSSAPIAuthentication yes
+    GSSAPIDelegateCredentials no
+    HostKeyAlgorithms ssh-rsa,ssh-dss
+
+Сохраняем изменения в файле и соединяемся по ssh.
